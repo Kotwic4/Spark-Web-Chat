c56cc93bd64482ed8419306c2a1d45e743737c70
Kotwic4 <Kotwic4@gmail.com>
2017-01-17 21:01:11 +0100

    add heroku

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static String defaultChannel = "Default";
 
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         channels.put(defaultChannel,new DefaultChannel());
         channels.put("chatbot",new ChatBotChannel());
         staticFiles.location("/public");
@@ -19,6 +20,14 @@ public class Main {
         get("/channels", (req, res) -> {
             return String.valueOf(new JSONObject().put("channelList", Main.channels.values()));
         });
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 
 }
